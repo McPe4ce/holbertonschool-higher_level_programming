1f98f11fff016cd9c25147955bc3a040dacdb337
Mcpe4ce <phil.ghanem@gmail.com>
2026-05-25 18:16:01 +0200

    Refactor CustomObject class by removing unnecessary whitespace and comments

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -9,12 +9,12 @@ class CustomObject:
         self.__name = name
         self.__age = age
         self.__is_student = is_student
-    
+
     def display(self):
         print(f"Name: {self.__name}")
         print(f"Age: {self.__age}")
         print(f"Is Student: {self.__is_student}")
-    
+
     def serialize(self, filename):
         try:
             with open(f"{filename}", "wb") as f:
@@ -23,8 +23,7 @@ class CustomObject:
             print(f"Cant write in {filename}")
         except pickle.PicklingError:
             print("Cant serialize the object")
-    
-    
+
     @classmethod
     def deserialize(cls, filename):
         try:
@@ -35,17 +34,3 @@ class CustomObject:
             print(f"Cant read {filename}")
         except pickle.UnpicklingError:
             print("Cant deserialize the object")
-
-
-# Create an instance of CustomObject
-obj = CustomObject(name="John", age=25, is_student=True)
-print("Original Object:")
-obj.display()
-
-# Serialize the object
-obj.serialize("object.pkl")
-
-# Deserialize the object into a new instance
-new_obj = CustomObject.deserialize("object.pkl")
-print("\nDeserialized Object:")
-new_obj.display()
