9b9b5ddd5f05c0087aa60612571ea654f3366e18
Mcpe4ce <phil.ghanem@gmail.com>
2026-05-26 14:14:00 +0200

    Refactor error handling in serialization functions to return False on failure

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -10,10 +10,9 @@ def serialize_and_save_to_file(data, filename="data.json"):
     try:
         with open(filename, "w", encoding="utf-8") as json_file:
             json.dump(data, json_file)
-    except (IOError, OSError) as e:
-        raise FileNotFoundError("Cant write in the file as it doesnt exist")
-    except TypeError as e:
-        raise TypeError("Data cannot be serialized")
+    except (IOError, OSError, TypeError):
+        return False
+    return True
 
 
 def convert_csv_to_json(filename):
@@ -22,9 +21,6 @@ def convert_csv_to_json(filename):
         with open(filename, newline="", encoding="utf-8") as csv_file:
             reader = csv.DictReader(csv_file)
             data = list(reader)
-    except (IOError, OSError):
-        raise FileNotFoundError("Cant write in the file as it doesnt exist")
-    except TypeError:
-        raise TypeError("Data cannot be serialized")
-    serialize_and_save_to_file(data, "data.json")
-    return True
+        return serialize_and_save_to_file(data, "data.json")
+    except (IOError, OSError, FileNotFoundError):
+        return False
