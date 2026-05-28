4b7a78132adadc58c273b92ffb9a8910a9f2357c
Mcpe4ce <phil.ghanem@gmail.com>
2026-05-28 11:46:55 +0200

    Refactor CustomObject attributes to use public access for serialization

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -7,14 +7,14 @@ import pickle
 class CustomObject:
     """Class that represents a custom object, will be serialized later"""
     def __init__(self, name, age, is_student):
-        self.__name = name
-        self.__age = age
-        self.__is_student = is_student
+        self.name = name
+        self.age = age
+        self.is_student = is_student
 
     def display(self):
-        print(f"Name: {self.__name}")
-        print(f"Age: {self.__age}")
-        print(f"Is Student: {self.__is_student}")
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Is Student: {self.is_student}")
 
     def serialize(self, filename):
         try:
