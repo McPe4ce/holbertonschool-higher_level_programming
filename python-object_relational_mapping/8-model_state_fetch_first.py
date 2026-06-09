82c6ef8171ff393fcbdd1c90b99580458af3f4e6
Mcpe4ce <phil.ghanem@gmail.com>
2026-06-09 16:10:57 +0200

    Bind metadata to engine in state fetching script

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -13,6 +13,7 @@ if __name__ == "__main__":
                 argv[1], argv[2], argv[3]),
             pool_pre_ping=True)
 
+    Base.metadata.bind = vroom
     Session = sessionmaker(bind=vroom)
     session = Session()
 
