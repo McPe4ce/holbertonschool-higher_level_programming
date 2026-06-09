01b3f140e7ab7768a8f1c88f9e6a5295e44cb333
Mcpe4ce <phil.ghanem@gmail.com>
2026-06-09 15:20:53 +0200

    Refactor engine and session creation in fetch_all script for better readability

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,18 +1,20 @@
 #!/usr/bin/python3
 """Query to fetch all the states using SQL alchemy"""
+
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from model_state import Base, State
 from sys import argv
 
-engine = create_engine(
-    "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-        argv[1], argv[2], argv[3]),
-    pool_pre_ping=True)
+if __name__ == "__main__":
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            argv[1], argv[2], argv[3]),
+        pool_pre_ping=True)
 
-Session = sessionmaker(bind=engine)
-session = Session()
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
 
-for da_state in session.query(State).order_by(State.id):
-    print(f"{da_state.id}: {da_state.name}")
+    for da_state in session.query(State).order_by(State.id):
+        print(f"{da_state.id}: {da_state.name}")
