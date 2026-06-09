b8b3562e58d243e878fb1427c8320fdc70bf4881
Mcpe4ce <phil.ghanem@gmail.com>
2026-06-09 21:21:26 +0200

    Add script to query state by name from the database

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    vroom = create_engine(
+            "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+                argv[1], argv[2], argv[3]),
+            pool_pre_ping=True)
+    
+    Session = sessionmaker(bind=vroom)
+    session = Session()
+
+    da_state_id = session.query(State).filter(
+        State.name.like(argv[4],))
+    
+    if da_state_id is None:
+        print("Not found")
+    else:
+        print(da_state_id.id)
+    
+    session.close()
