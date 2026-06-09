041615a24dc9acc1f29a71dd05f5125b8c0fbd6c
Mcpe4ce <phil.ghanem@gmail.com>
2026-06-09 14:16:28 +0200

    Refactor model_state.py by removing unused imports and engine creation

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sys import argv
-
-engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(argv[1], argv[2], argv[3]), pool_pre_ping=True)
 
 Base = declarative_base()
 
