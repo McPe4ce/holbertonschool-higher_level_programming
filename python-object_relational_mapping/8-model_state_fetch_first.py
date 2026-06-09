f297ac4fbb59c9784cce1277291e3375d6710ee2
Mcpe4ce <phil.ghanem@gmail.com>
2026-06-09 16:29:32 +0200

    Refactor script to improve clarity and consistency in database connection and state fetching

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,25 +1,23 @@
 #!/usr/bin/python3
-"""Script that prints the first State object from the db"""
+"""Prints the first State object from the database hbtn_0e_6_usa."""
 
-from sys import argv
-from sqlalchemy.orm import sessionmaker
+
+import sys
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
+
 if __name__ == "__main__":
-
-    vroom = create_engine(
-            "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-                argv[1], argv[2], argv[3]),
-            pool_pre_ping=True)
-
-    Base.metadata.bind = vroom
-    Session = sessionmaker(bind=vroom)
-    session = Session()
-
-    da_state = session.query(State).order_by(State.id).first()
-
-    if da_state is None:
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.bind = engine
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
+    state = session.query(State).order_by(State.id).first()
+    if state is None:
         print("Nothing")
     else:
-        print(f"{da_state.id}: {da_state.name}")
+        print("{}: {}".format(state.id, state.name))
+    session.close()
