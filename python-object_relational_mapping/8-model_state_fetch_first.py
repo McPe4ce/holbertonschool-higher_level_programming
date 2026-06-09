24d566376ca3cd3fba3bb511e2d57a39bdeb6a75
Mcpe4ce <phil.ghanem@gmail.com>
2026-06-09 15:37:40 +0200

    Refactor main block to encapsulate database connection and session creation

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -6,17 +6,19 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 from model_state import Base, State
 
-vroom = create_engine(
-        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
-            argv[1], argv[2], argv[3]),
-        pool_pre_ping=True)
+if __name__ == "__main__":
 
-Session = sessionmaker(bind=vroom)
-session = Session()
+    vroom = create_engine(
+            "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+                argv[1], argv[2], argv[3]),
+            pool_pre_ping=True)
 
-if not State:
-    print("Nothing\n")
-else:
-    for da_state in session.query(State).\
-            filter(State.id == 1):
-        print(f"{da_state.id}: {da_state.name}")
+    Session = sessionmaker(bind=vroom)
+    session = Session()
+
+    if not State:
+        print("Nothing\n")
+    else:
+        for da_state in session.query(State).\
+                filter(State.id == 1):
+            print(f"{da_state.id}: {da_state.name}")
