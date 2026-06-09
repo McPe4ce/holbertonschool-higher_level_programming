f20c97144075b71e3ecfde8c6fb5182ef16be4c3
Mcpe4ce <phil.ghanem@gmail.com>
2026-06-09 14:22:45 +0200

    Fix docstring indentation in the State class

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -7,7 +7,7 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 class State(Base):
-    
+    """Class for the states table in the SQL"""
     __tablename__ = 'states'
 
     id = Column(Integer, primary_key=True, nullable=False)
