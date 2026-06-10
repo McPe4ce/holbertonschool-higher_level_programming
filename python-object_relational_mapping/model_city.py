eca4440fe25c73d4a8b93823480d5376e77ee703
Mcpe4ce <phil.ghanem@gmail.com>
2026-06-10 14:19:23 +0200

    Fix formatting and add docstring for the City class in model_city.py

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-
+"""Module that creates the city class for the cities table"""
 from sqlalchemy import Integer, String, Column, ForeignKey
 from model_state import Base, State
 
+
 class City(Base):
-    
+
     __tablename__ = 'cities'
 
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey(State.id), nullable=False)
-
