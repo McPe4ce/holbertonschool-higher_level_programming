fd0663414a287957bbee3ba02411ae034e9f0e61
Mcpe4ce <phil.ghanem@gmail.com>
2026-06-12 10:20:50 +0200

    Add /info endpoint to return API version and description

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -28,11 +28,21 @@ class ApiSimple(BaseHTTPRequestHandler):
             self.end_headers()
             self.wfile.write(b"OK")
 
-        else:
-            self.send_response(404)
+        elif self.path == "/info":
+            self.send_response(200)
             self.send_header("Content-Type", "application/json")
             self.end_headers()
-            self.wfile.write(json.dumps({"error": "Not found"}).encode("utf-8"))
+            info = {
+                "version": "1.0",
+                "description": "A simple API built with http.server"
+            }
+            self.wfile.write(json.dumps(info).encode("utf-8"))
+
+        else:
+            self.send_response(404)
+            self.send_header("Content-Type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Endpoint not found")
 
 
 if __name__ == "__main__":
