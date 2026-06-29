3d72ce74790208db57c5f7d3d945f3446566c3a9
Mcpe4ce <phil.ghanem@gmail.com>
2026-06-29 11:41:27 +0200

    Refactor 7-multi_c.js to simplify occurrence validation and streamline output logic

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,16 +1,12 @@
 #!/usr/bin/node
 
 const x = process.argv[2];
-if (x === undefined) {
+if (x === undefined || !Number.isInteger(parseInt(x))) {
   console.log('Missing number of occurrences');
 }
 
 let index;
 
 for (index = 0; index < x; index++) {
-  if (!Number.isInteger(parseInt(x))) {
-    console.log('Missing number of occurrences');
-  } else {
-    console.log('C is fun');
-  }
+  console.log('C is fun');
 }
