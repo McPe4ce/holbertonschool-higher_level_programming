cefcf5084a955b1603d4e0dea6bca2f1b0be27e1
Mcpe4ce <phil.ghanem@gmail.com>
2026-06-29 10:26:48 +0200

    Add multi-language greeting script in JavaScript

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+const myc = 'C is fun';
+const mypyth = 'Python is cool';
+const myJava = 'Javascript is amazing';
+
+console.log(myc);
+console.log(mypyth);
+console.log(myJava);
