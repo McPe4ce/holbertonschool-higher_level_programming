8d5cab43574347b1ea281123ddbad8c1791c2b45
Mcpe4ce <phil.ghanem@gmail.com>
2026-06-29 11:31:05 +0200

    Fix syntax error by adding missing semicolon and adjusting indentation in 6-multi_languages_loop.js

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 
-const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing']
+const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let index;
 
 for (index = 0; index < myArray.length; index++) {
-    console.log(myArray[index]);
+  console.log(myArray[index]);
 }
